92024e06147ad95cde98481f7d493caca87a3b09
iua.suranov <pradedura@mail.ru>
2025-09-23 23:41:37 +0300

    fix many files

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-PATH_DATA = BASE_DIR / "data" / "operations.xlsx"
+PATH_XLSX = BASE_DIR / "data" / "operations.xlsx"
+
+PATH_DATA_USER_SET = BASE_DIR / "data" / "user_settings.json"
 
 PATH_LOGS = BASE_DIR / "logs"
 
